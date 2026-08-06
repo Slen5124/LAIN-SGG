97c06ef309d82fa5821f40ce2263a888d6129679
Yunhan <yunhan26@cv-knu.local>
2026-08-06 07:33:37 +0000

    Use generic object proposals for VG

--- models/LAIN.py
+++ models/LAIN.py
@@ -325,32 +325,44 @@ class LAIN(nn.Module):
 
             keep = torch.nonzero(sc >= self.box_score_thresh).squeeze(1)
 
-            is_human = lb == self.human_idx
-            hum = torch.nonzero(is_human).squeeze(1)
-            obj = torch.nonzero(is_human == 0).squeeze(1)
-            n_human = is_human[keep].sum(); n_object = len(keep) - n_human
-            # Keep the number of human and object instances in a specified interval
-            if n_human < self.min_instances:
-                keep_h = sc[hum].argsort(descending=True)[:self.min_instances]
-                keep_h = hum[keep_h]
-            elif n_human > self.max_instances:
-                keep_h = sc[hum].argsort(descending=True)[:self.max_instances]
-                keep_h = hum[keep_h]
+            if self.dataset == "vg":
+                # SGG allows every object class to act as either subject or
+                # object. Select one generic proposal set without imposing
+                # the original LAIN human/object quotas.
+                if len(keep) < self.min_instances:
+                    keep = sc.argsort(descending=True)[:self.min_instances]
+                elif len(keep) > self.max_instances:
+                    order = sc[keep].argsort(descending=True)
+                    keep = keep[order[:self.max_instances]]
             else:
-                keep_h = torch.nonzero(is_human[keep]).squeeze(1)
-                keep_h = keep[keep_h]
+                # Original LAIN proposal selection for HICO-DET/V-COCO.
+                is_human = lb == self.human_idx
+                hum = torch.nonzero(is_human).squeeze(1)
+                obj = torch.nonzero(is_human == 0).squeeze(1)
+                n_human = is_human[keep].sum()
+                n_object = len(keep) - n_human
 
-            if n_object < self.min_instances:
-                keep_o = sc[obj].argsort(descending=True)[:self.min_instances]
-                keep_o = obj[keep_o]
-            elif n_object > self.max_instances:
-                keep_o = sc[obj].argsort(descending=True)[:self.max_instances]
-                keep_o = obj[keep_o]
-            else:
-                keep_o = torch.nonzero(is_human[keep] == 0).squeeze(1)
-                keep_o = keep[keep_o]
+                if n_human < self.min_instances:
+                    keep_h = sc[hum].argsort(descending=True)[:self.min_instances]
+                    keep_h = hum[keep_h]
+                elif n_human > self.max_instances:
+                    keep_h = sc[hum].argsort(descending=True)[:self.max_instances]
+                    keep_h = hum[keep_h]
+                else:
+                    keep_h = torch.nonzero(is_human[keep]).squeeze(1)
+                    keep_h = keep[keep_h]
 
-            keep = torch.cat([keep_h, keep_o])
+                if n_object < self.min_instances:
+                    keep_o = sc[obj].argsort(descending=True)[:self.min_instances]
+                    keep_o = obj[keep_o]
+                elif n_object > self.max_instances:
+                    keep_o = sc[obj].argsort(descending=True)[:self.max_instances]
+                    keep_o = obj[keep_o]
+                else:
+                    keep_o = torch.nonzero(is_human[keep] == 0).squeeze(1)
+                    keep_o = keep[keep_o]
+
+                keep = torch.cat([keep_h, keep_o])
 
             region_props.append(dict(
                 boxes=bx[keep],
